58b5528ea3e3923e6e3df800d7de7cd3635612b8
Justin Fyfe <justin@fyfesoftware.ca>
2022-09-14 17:34:44 -0400

    Implemented persistence dataset installer from multiple sources and store of the applied patches in the database SCOM-113 SCOM-62 #close

---
+++ SanteDB.Core.Applets/Services/Impl/AppletDatasetProvider.cs
@@ -0,0 +1,45 @@
+﻿using SanteDB.Core.Data.Initialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SanteDB.Core.Applets.Services.Impl
+{
+    /// <summary>
+    /// An implementation of the <see cref="IDatasetProvider"/> which loads dataset files from applets
+    /// </summary>
+    public class AppletDatasetProvider : IDatasetProvider
+    {
+        private readonly IAppletManagerService m_appletManager;
+        private readonly IAppletSolutionManagerService m_solutionManager;
+
+        /// <summary>
+        /// DI constructor
+        /// </summary>
+        public AppletDatasetProvider(IAppletManagerService appletManager, IAppletSolutionManagerService solutionManager)
+        {
+            this.m_appletManager = appletManager;
+            this.m_solutionManager = solutionManager;
+        }
+
+        /// <summary>
+        /// Get all datasets
+        /// </summary>
+        public IEnumerable<Dataset> GetDatasets()
+        {
+            return this.m_solutionManager.Solutions
+                .SelectMany(o => this.m_solutionManager.GetApplets(o.Meta.Id))
+                .Union(this.m_appletManager.Applets)
+                .SelectMany(o => o.Assets.Where(a => a.Name.EndsWith(".dataset")))
+                .Select(o =>
+                {
+                    using (var ms = new MemoryStream(this.m_appletManager.Applets.RenderAssetContent(o)))
+                    {
+                        return Dataset.Load(ms);
+                    }
+                });
+        }
+    }
+}
